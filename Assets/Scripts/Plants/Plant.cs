a01870e3e9943426b1f18e4cfedde56627ad2c2b
ahmed alnour <ahmed2699@gmail.com>
2022-05-09 17:39:10 +0200

    Phase 2

--- Assets/Scripts/Plants/Plant.cs
+++ Assets/Scripts/Plants/Plant.cs
@@ -1,14 +1,26 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Plant : MonoBehaviour {
-    private UnityEvent touchedEnemy = default;
-    private UnityEvent died = default;
+    // private UnityEvent touchedEnemy = default;
+    [HideInInspector]
+    public UnityEvent died = default;
 
+    public GameObject bullet;
     public float upwardOffset;
+    public int maxHealth;
+    public int attackDamage;
+    public int attackTimeout;
+
+
+    private int currentHealth;
 
     void Start() {
         transform.position += Vector3.up * upwardOffset;
+        currentHealth = maxHealth;
+
+        StartCoroutine(Attack());
     }
 
 
@@ -16,9 +28,31 @@ public class Plant : MonoBehaviour {
 
     }
 
+    private IEnumerator Attack() {
+        while (true) {
+            var _bullet = Instantiate(bullet, transform.position, Quaternion.identity).GetComponent<Bullet>();
+            _bullet.damage = attackDamage;
+            yield return new WaitForSeconds(attackTimeout);
+        }
+    }
+
+    public void TakeDamage(int damage) {
+        // play hit animation
+        currentHealth -= damage;
+        if (currentHealth <= 0) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        // play death animation
+        died?.Invoke();
+        Destroy(gameObject);
+    }
+
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Enemy")) {
-            touchedEnemy?.Invoke();
+            // touchedEnemy?.Invoke();
         }
     }
 }
