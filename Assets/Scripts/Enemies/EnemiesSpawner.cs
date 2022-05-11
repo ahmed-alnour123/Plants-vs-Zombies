33600fb27e828d6099dd439fa12123b7aa873442
ahmed alnour <ahmed2699@gmail.com>
2022-05-12 01:33:27 +0200

    Phase 5

--- Assets/Scripts/Enemies/EnemiesSpawner.cs
+++ Assets/Scripts/Enemies/EnemiesSpawner.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemiesSpawner : MonoBehaviour {
 
     public float timeSleep;
     public List<GameObject> enemies;
     public List<Transform> spawnPoints;
+    public Slider waveSlider;
 
     [HideInInspector]
     public bool canSpawn = true;
 
     void Start() {
         StartCoroutine(Spawn());
+    }
+
+    private void Update() {
+        // waveSlider.value = waveCounter;
     }
 
     IEnumerator Spawn() {
